67dcadc588238a01f3ce4df98b3ac2891b938022
tctcab <ztc.ustcer@gmail.com>
2020-11-05 23:37:14 +1000

    split environment.sh out

--- scripts/set_environments.sh
+++ scripts/set_environments.sh
@@ -1,13 +1,27 @@
-# check required scripts
-# please source this script before run_rnacmap.sh
+# environment settings
 # DO NOT MOVE
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RNACMAP_DIR=$(dirname $SCRIPT_DIR)
 
 
-path_blastn=./ncbi-blast-2.10.0+/bin
-path_blastn_database=./nt_database/nt
-path_infernal=./infernal-1.1.3-linux-intel-gcc/binaries
-path_infernal_database=./nt_database/nt
+
+# path for ncbi-blast
+path_blastn=/project/aspen/software/ncbi-blast-2.2.30+/bin
+path_blastn_database=/project/aspen/TC/GIT/DATA/NT/nt/nt
+
+#path for infernal 1.1.3
+path_infernal=${RNACMAP_DIR}/3rd_party/infernal/infernal-1.1.3-linux-intel-gcc/binaries
+path_infernal_database=/project/aspen/TC/DATA/NT/fasta/nt
+
+#path for SPOT-RNA
+path_spotrna=$RNACMAP_DIR/3rd_party/SPOT-RNA
+
+# Required for mfdca
 path_matlab=/usr/local/bin
+
+export PATH=${RNACMAP_DIR}/bin:$PATH
+
+export PATH=${path_infernal}:$PATH
+
+export PATH=${path_blastn}:$PATH
